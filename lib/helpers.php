f278557da74c2396852dc546327e65a20a0806bb
Olivier Laviale <contact@weirdog.com>
2014-03-17 15:43:49 +0100

    Added generate_v4_uuid

--- lib/helpers.php
+++ lib/helpers.php
@@ -557,3 +557,18 @@ function normalize_url_path($path)
 
 	return $normalized;
 }
+
+/**
+ * Generates a v4 UUID.
+ *
+ * @return string
+ *
+ * @origin http://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid#answer-15875555
+ */
+function generate_v4_uuid()
+{
+	$data = openssl_random_pseudo_bytes(16);
+	$data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0010
+	$data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+	return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
